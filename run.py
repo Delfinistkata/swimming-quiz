ed4ac4e691353c70f1b4edad01ccdd73e9b8533e
Delfinistkata <elidimitrovadkit@gmail.com>
2023-03-27 20:39:45 +0000

    Wire up APIs and confirm access to Google sheets

--- run.py
+++ run.py
@@ -1,1 +1,17 @@
+import gspread
+from google.oauth2.service_account import Credentials
 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('score_board')
+
+score = SHEET.worksheet('total_score')
+data = score.get_all_values()
+print(data)
