e26b6d805aed2e80ca105523bb0c200e7c51ef6c
Delfinistkata <elidimitrovadkit@gmail.com>
2023-03-30 13:15:45 +0000

    Add menu and main function to call the welcome message and the logo

--- run.py
+++ run.py
@@ -75,7 +75,56 @@ def welcome_message():
 
     print(Style.RESET_ALL + 'Welcome to the swimming quiz!\n')
     print('Are you ready to test you knowledge about swimming?\n')
-    time.sleep(5)
+    input("Press Enter to continue...")
 
 
-clear_board()
+def menu():
+    '''
+    A menu with three options for the player to choose from
+    '''
+    print("[1] Rules")
+    print("[2] Play")
+    print("[0] Quit")
+
+
+menu()
+option = int(input("Enter your option here: "))
+
+while True:
+    try:
+        option = int(input("Enter your option here: "))
+        if option not in [1, 2, 0]:
+            raise ValueError("Invalid Option! Enter: 1, 2, or 0")
+        if option == 1:
+            rules = [
+                "The player is presented with a question and possible answers",
+                "The player answers by entering the correct letter.",
+                "If the player selects the correct answer, they earn a point.",
+                "If the answer is incorrect, no points are earned.",
+                "The game proceeds with new questions until all are answered.",
+                "At the end of the game, the player's score is displayed."
+            ]
+            for rule in rules:
+                for line in textwrap.wrap(rule, width=79):
+                    print(line)
+                time.sleep(2)
+        elif option == 2:
+            print("Play")
+        else:
+            print("Thank you for playing. See you soon!")
+            break
+    except ValueError as e:
+        print(e)
+
+
+def main_functions():
+    '''
+    Calls the main functions on the terminal
+    '''
+    clear_board()
+    welcome_message()
+    menu()
+    option = int(input("Enter your option here: "))
+
+
+main_functions()
